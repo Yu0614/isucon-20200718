c10cccfc4efd879ba2bbd66584d59ba3bfe2d7fc
Yu0614 <azu369yu@gmail.com>
2020-07-18 15:22:40 +0900

    feat: add index into database (for memos.created_at & memos.is_private)

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+# Add index into database (for memos.created_at & memos.is_private)
+echo 'show databases; use isucon; ALTER TABLE memos ADD INDEX index_on_created_and_is_private(created_at, is_private);' | mysql -uisucon -pisucon
