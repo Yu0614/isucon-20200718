16127d579c77c2de639e5acf2723cba081281e73
IkezoeMakoto <150283t@gmail.com>
2020-07-18 14:41:16 +0900

    pprof入れた

--- app/src/app.go
+++ app/src/app.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/securecookie"
+	"github.com/gomarkdown/markdown"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -91,19 +92,8 @@ var (
 			return session.Values["token"]
 		},
 		"gen_markdown": func(s string) template.HTML {
-			f, _ := ioutil.TempFile(tmpDir, "isucon")
-			defer f.Close()
-			f.WriteString(s)
-			f.Sync()
-			finfo, _ := f.Stat()
-			path := tmpDir + finfo.Name()
-			defer os.Remove(path)
-			cmd := exec.Command(markdownCommand, path)
-			out, err := cmd.Output()
-			if err != nil {
-				log.Printf("can't exec markdown command: %v", err)
-				return ""
-			}
+			md := []byte(s)
+			out := markdown.ToHTML(md, nil, nil)
 			return template.HTML(out)
 		},
 	}
